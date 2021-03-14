19fea90645cb7c6a2c202b5db523433d6afd6f78
NCPlyn <jakobabc@seznam.cz>
2021-03-14 21:07:59 +0100

    time, new stuff

--- index.js
+++ index.js
@@ -10,8 +10,16 @@ const io = socketio(server);
 app.use(express.static(path.join(__dirname, 'public')));
 
 io.on('connection', (socket) => {
-    socket.on('chat',msg => {
-        io.emit('chat', msg);
+    socket.on('chat',(msgbox, author) => {
+      let ts = Date.now();
+      let date_ob = new Date(ts);
+      let date = date_ob.getDate();
+      let month = date_ob.getMonth() + 1;
+      let year = date_ob.getFullYear();
+      let hours = date_ob.getHours();
+      let minutes = date_ob.getMinutes();
+      let dateout = hours + ":" + minutes + " " + year + "." + month + "." + date;
+      io.emit('chat', msgbox, author, dateout);
     });
 });
 
--- public/main.js
+++ public/main.js
@@ -1,13 +1,14 @@
 const socket = io();
-const message = document.getElementById('message');
+const msgbox = document.getElementById('msgbox');
+const author = document.getElementById('author');
 const send = document.getElementById('send');
 const chatbox = document.getElementById('chatbox');
 
 send.addEventListener('click', function(e) {
-    if(message.value)
-        socket.emit('chat', message.value);
+    if(msgbox.value && author.value)
+        socket.emit('chat', msgbox.value, author.value);
 });
 
-socket.on('chat', msg => {
-    chatbox.innerHTML = msg;
+socket.on('chat', (msgbox, author, dateout) => {
+    chatbox.innerHTML += "Timestamp: "+dateout+" Author: " + author + " Message: " + msgbox + "<br>";
 });
