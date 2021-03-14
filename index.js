b3695eec32a2ef2e1cb65f7b7965ca51da660acb
NCPlyn <jakobabc@seznam.cz>
2021-03-14 21:54:59 +0100

    save to json and load

--- index.js
+++ index.js
@@ -12,7 +12,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 io.on('connection', (socket) => {
   let data = JSON.parse(fs.readFileSync("data.json", 'utf8'));
-  data.forEach(function(obj) { io.emit('chat', obj.author, obj.msgbox, obj.timestamp); });
+  data.forEach(function(obj) {
+    io.emit('chat', obj.author, obj.msgbox, obj.timestamp, "1");
+  });
+  io.emit('loading', "1");
     socket.on('chat',(msgbox, author) => {
       let ts = Date.now();
       let date_ob = new Date(ts);
@@ -22,7 +25,16 @@ io.on('connection', (socket) => {
       let hours = date_ob.getHours();
       let minutes = date_ob.getMinutes();
       let dateout = hours + ":" + minutes + " " + year + "." + month + "." + date;
-      io.emit('chat', author, msgbox, dateout);
+      data.push({"author":author,"msgbox":msgbox,"timestamp":dateout});
+      jsonStr = JSON.stringify(data, null, 2);
+      fs.writeFile("data.json", jsonStr, err => {
+        if (err) {
+          console.log(`Data couldn't be saved! Error: ${err}`);
+        } else {
+          console.log(`Data was saved successfully`);
+        }
+      });
+      io.emit('chat', author, msgbox, dateout, "0");
     });
 });
 
--- public/main.js
+++ public/main.js
@@ -3,12 +3,19 @@ const msgbox = document.getElementById('msgbox');
 const author = document.getElementById('author');
 const send = document.getElementById('send');
 const chatbox = document.getElementById('chatbox');
+let load = "0";
 
 send.addEventListener('click', function(e) {
     if(msgbox.value && author.value)
         socket.emit('chat', msgbox.value, author.value);
 });
 
-socket.on('chat', (author, msgbox, dateout) => {
+socket.on('chat', (author, msgbox, dateout, loading) => {
+  if(loading == "0" || loading != load) {
     chatbox.innerHTML += "Timestamp: "+dateout+" Author: " + author + " Message: " + msgbox + "<br>";
+  }
+});
+
+socket.on('loading', loaded => {
+  load = loaded;
 });
