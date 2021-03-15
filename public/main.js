799ebbb6d143455e7f35888da67e7178b99b99e4
NCPlyn <jakobabc@seznam.cz>
2021-03-15 12:58:00 +0100

    fix bug with loading

--- public/main.js
+++ public/main.js
@@ -3,22 +3,23 @@ const msgbox = document.getElementById('msgbox');
 const author = document.getElementById('author');
 const scrp = document.getElementById('scrp');
 const chatbox = document.getElementById('chatbox');
-let load = "0";
+let loaded = "0";
 
 socket.on('chat', (author, msgbox, dateout, loading, image) => {
-  console.log(author+", "+msgbox+", "+dateout+", "+loading+", "+image)
-  if(loading == "0" || loading != load && image == "none") {
-    console.log("without")
-    $("#chatbox").prepend("<div class='post'><div class='who'><span class='whoname'>"+author+" &nbsp; &nbsp;</span><span>"+dateout+"</span></div><blockquote>"+msgbox+"</blockquote></div>");
-  } else if(loading == "0" || loading != load && image != "none") {
-    console.log("with")
-    let rando = Math.ceil(Math.random() * 50000);
-    $("#chatbox").prepend("<div class='post'><div class='who'><span class='whoname'>"+author+" &nbsp; &nbsp;</span><span>"+dateout+"</span></div><blockquote>"+msgbox+"</blockquote><div class='imag' id='"+rando+"'><img src='"+image+"'></div></div>");
+  if(loading == "0" || loading != loaded) {
+    if(image == "none") {
+      console.log("WITHOUTOUT IMAGE")
+      $("#chatbox").prepend("<div class='post'><div class='who'><span class='whoname'>"+author+" &nbsp; &nbsp;</span><span>"+dateout+"</span></div><blockquote>"+msgbox+"</blockquote></div>");
+    } else {
+      console.log("WITH IMAGE")
+      let rando = Math.ceil(Math.random() * 50000);
+      $("#chatbox").prepend("<div class='post'><div class='who'><span class='whoname'>"+author+" &nbsp; &nbsp;</span><span>"+dateout+"</span></div><blockquote>"+msgbox+"</blockquote><div class='imag' id='"+rando+"'><img src='"+image+"'></div></div>");
+    }
   }
 });
 
-socket.on('loading', loaded => {
-  load = "1";
+socket.on('loading', load => {
+  loaded = "1";
 });
 
 document.addEventListener('click',function(e){
