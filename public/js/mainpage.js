9b0d976e171ac742c66307ed2b716e1540594ff0
NCPlyn <jakobabc@seznam.cz>
2021-03-23 21:58:13 +0100

    better use of socketio

--- public/js/mainpage.js
+++ public/js/mainpage.js
@@ -2,25 +2,13 @@ const socket = io();
 const boardsbox = document.getElementById('boardsbox');
 const createform = document.getElementById('createform');
 const showform = document.getElementById('showform');
-let loaded = "0";
 createform.style.display = "none";
-let userpagenum = Math.ceil(Math.random() * 20000);
 
-socket.on('mainload', (boardid, name, desc, image, loading, useridback) => { //gets data from node.js and adds it to html, checks if we are loading or not
-  if (useridback == userpagenum) {
-    if (loading == "0" || loading != loaded) {
-      boardsbox.innerHTML += "<div class='col-lg-3 col-sm-6 '><div class='card'><img class='card-img-top' src='" + image + "'><div class='card-body'><a class='card-title text-center' href=boards.html?id=" + boardid + ">" + name + "</a><p class='card-text'>" + desc + "</p></div></div></div>"
-    }
-  }
+socket.on('mainload', (boardid, name, desc, image) => { //gets data from node.js and adds it to html
+  boardsbox.innerHTML += "<div class='col-lg-3 col-sm-6 '><div class='card'><img class='card-img-top' src='" + image + "'><div class='card-body'><a class='card-title text-center' href=boards.html?id=" + boardid + ">" + name + "</a><p class='card-text'>" + desc + "</p></div></div></div>"
 });
 
-socket.on('mainloading', useridback => { //get loaded signal
-  if (useridback == userpagenum) {
-    loaded = "1";
-  }
-});
-
-socket.emit('getmain', userpagenum); //send signal that we want to load boards
+socket.emit('getmain'); //send signal that we want to load boards
 
 showform.addEventListener('click', function(e) { //listener to show/hide form
   if (createform.style.display === "none") {
